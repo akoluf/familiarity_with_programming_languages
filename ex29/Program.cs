602106ffd485f6894e268046a7332cc100339f62
akoluf <Rifald478@gmail.com>
2023-08-08 19:49:52 +0300

    Оформил решение задачи 29

--- ex29/Program.cs
+++ ex29/Program.cs
@@ -6,9 +6,23 @@ int[] GetMassiv(int count)
     int[] arr = new int[count];
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(0, 2);
+        Console.Write($"Введите число {i + 1}: ");
+        arr[i] = int.Parse(Console.ReadLine() ?? "");
     }
     return arr;
 }
 
 void PrintArr(int[] array)
+{
+    Console.Write("(");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.Write(")");
+}
+
+PrintArr(GetMassiv(8));
