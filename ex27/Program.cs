0fddc7c1c95bb496b74c886a56cea575858ce9f8
akoluf <Rifald478@gmail.com>
2023-08-08 19:35:50 +0300

    Добавил папку ex27 и начал оформление ее решения

---
+++ ex27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Написать программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
+int sumNumbers = GetSumNumbers(num);
+Console.WriteLine($"{num} -> {sumNumbers}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetSumNumbers(int number)
