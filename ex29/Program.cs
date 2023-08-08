cf78bea2c7687daf85ff041514fdfdb06c8dc2e8
akoluf <Rifald478@gmail.com>
2023-08-08 19:47:27 +0300

    добавил папку ex29 и начал оформлять решение задачи

---
+++ ex29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 29: Написать программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+
+int[] GetMassiv(int count)
+{
+    int[] arr = new int[count];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+void PrintArr(int[] array)
