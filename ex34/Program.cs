de272172bdd06fc4d67a22b57a435cf0888688fd
akoluf <Rifald478@gmail.com>
2023-08-13 20:43:51 +0300

    завершил решение задачи ex34

--- ex34/Program.cs
+++ ex34/Program.cs
@@ -2,5 +2,37 @@
 // положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 Console.Clear();
 
-int[] array = GetArray(12, 1, 2000);
+int[] array = GetArray(12, 100, 999);
+Console.WriteLine("Сгенерированный массив:");
 Console.WriteLine(String.Join(" ", array));
+
+int evenCount = GetEvenCount(array);
+Console.WriteLine($"Четных чисел: {evenCount}");
+
+static int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    Random random = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = random.Next(minValue, maxValue + 1);
+    }
+
+    return res;
+}
+
+static int GetEvenCount(int[] arr)
+{
+    int evenCount = 0;
+
+    foreach (int el in arr)
+    {
+        if (el % 2 == 0)
+        {
+            evenCount++;
+        }
+    }
+
+    return evenCount;
+}
