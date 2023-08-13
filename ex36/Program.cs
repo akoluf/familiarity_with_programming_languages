f794fa5ec2f5ef5b2c4061262a04117fe503d9dc
akoluf <Rifald478@gmail.com>
2023-08-13 20:51:55 +0300

    завершил решение задачи ex36

--- ex36/Program.cs
+++ ex36/Program.cs
@@ -1,6 +1,34 @@
 ﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 Console.Clear();
 
-int[] array = GetArray(12, 100, 999);
+int[] array = GetArray(20, -200, 200);
 Console.WriteLine("Сгенерированный массив:");
 Console.WriteLine(String.Join(" ", array));
+
+int oddPositionSum = GetOddPositionSum(array);
+Console.WriteLine($"Сумма элементов на нечетных позициях: {oddPositionSum}");
+
+static int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    Random random = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = random.Next(minValue, maxValue + 1);
+    }
+
+    return res;
+}
+
+static int GetOddPositionSum(int[] arr)
+{
+    int oddPositionSum = 0;
+
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        oddPositionSum += arr[i];
+    }
+
+    return oddPositionSum;
+}
