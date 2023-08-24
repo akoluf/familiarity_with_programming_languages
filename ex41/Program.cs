5c9b3fd212d035ecdbc0c362bf7acaede3d42447
akoluf <Rifald478@gmail.com>
2023-08-24 16:12:38 +0300

    оформил решение

--- ex41/Program.cs
+++ ex41/Program.cs
@@ -1,1 +1,18 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+
+Console.Write("Введите числа, разделяя их пробелами: ");
+string input = Console.ReadLine() ??"";
+
+string[] numbers = input.Split(' ');
+
+Console.WriteLine("Массив чисел: ");
+foreach (string number in numbers)
+{
+    Console.Write(number + " ");
+}
+
+int count = numbers.Length;
+Console.WriteLine($"\nБыло введено {count} чисел.");
+
+Console.ReadLine();
