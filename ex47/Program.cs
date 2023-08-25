4506ab540a011578cddb5e8096ede0a8be450ab4
akoluf <Rifald478@gmail.com>
2023-08-25 19:28:40 +0300

    добавил папку с заданием ex47 и начал его оформление

---
+++ ex47/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
