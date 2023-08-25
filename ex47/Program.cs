7cc943141b55d3dc17ded1643ffcbbabebc8a98d
akoluf <Rifald478@gmail.com>
2023-08-25 19:42:32 +0300

    оформил решение ex47

--- ex47/Program.cs
+++ ex47/Program.cs
@@ -7,6 +7,38 @@ int rows = int.Parse(Console.ReadLine() ?? "");
 Console.Write("Введите количество столбцов массива: ");
 int columns = int.Parse(Console.ReadLine() ?? "");
 
-int[,] array = GetArray(rows, columns, 0, 10);
+double[,] array = GetArray(rows, columns, -10, 10);
 PrintArray(array);
 
+
+static double[,] GetArray(int m, int n, double minValue, double maxValue)
+{
+    double[,] result = new double[m, n];
+    Random random = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double value = Math.Round((random.NextDouble() * (maxValue - minValue) + minValue), 2);
+            result[i, j] = random.Next(2) == 0 ? value : -value; // Случайный выбор знака числа
+        }
+    }
+
+    return result;
+}
+
+static void PrintArray(double[,] inArray)
+{
+    int m = inArray.GetLength(0);
+    int n = inArray.GetLength(1);
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write($"{inArray[i, j]:0.00} ");
+        }
+        Console.WriteLine();
+    }
+}
