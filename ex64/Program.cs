c70f6cdcc2d0ee3a1eaaac05e52d59092e2d5998
akoluf <Rifald478@gmail.com>
2023-09-02 17:57:35 +0300

    добавил папку с заданием ex64 и начал его оформление

---
+++ ex64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до M.
+
+Console.Clear();
+Console.Write("Введите число M: ");
+if (!int.TryParse(Console.ReadLine(), out int m))
+{
+    Console.WriteLine("Некорректный ввод числа M. Пожалуйста, введите целое число.");
+    return;
+}
+
+Console.Write("Введите число N: ");
+if (!int.TryParse(Console.ReadLine(), out int n))
+{
+    Console.WriteLine("Некорректный ввод числа N. Пожалуйста, введите целое число.");
+    return;
+}
