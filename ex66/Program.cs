6340338e0230a093d618715881597a95c0321de3
akoluf <Rifald478@gmail.com>
2023-09-02 18:13:22 +0300

    оформил решение ex66

--- ex66/Program.cs
+++ ex66/Program.cs
@@ -13,3 +13,21 @@ if (!int.TryParse(Console.ReadLine(), out int n))
     Console.WriteLine("Некорректный ввод числа N. Пожалуйста, введите целое число.");
     return;
 }
+
+if (m >= n)
+{
+    Console.WriteLine("Число N должно быть больше числа M.");
+    return;
+}
+
+int sum = GetNaturSum(m, n);
+
+Console.WriteLine("Сумма натуральных чисел: " + sum);
+Console.WriteLine("...............");
+
+static int GetNaturSum(int startNum, int endNum)
+{
+    if (startNum == endNum) return startNum;
+
+    return startNum + GetNaturSum(startNum + 1, endNum);
+}
