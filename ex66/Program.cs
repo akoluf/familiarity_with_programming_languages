15e651183c539addd26c79de3980336d4cf04a1e
akoluf <Rifald478@gmail.com>
2023-09-02 18:08:50 +0300

    добавил папку с заданием ex66 и начал его оформление

---
+++ ex66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Clear();
+Console.Write("Введите число M: ");
+if (!int.TryParse(Console.ReadLine(), out int m))
+{
+    Console.WriteLine("Некорректный ввод числа M. Пожалуйста, введите целое число.");
+    return;
+}
+
+Console.Write("Введите число N: ");
+if (!int.TryParse(Console.ReadLine(), out int n))
+{
+    Console.WriteLine("Некорректный ввод числа N. Пожалуйста, введите целое число.");
+    return;
+}
